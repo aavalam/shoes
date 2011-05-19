aa079216b60349dd8d34b75a6ec49e3af99a1a84
Victor Goff <keeperotphones@gmail.com>
2011-05-19 16:24:32 -0500

    added doc directory to ignore file

--- test/shoes_test.rb
+++ test/shoes_test.rb
@@ -5,4 +5,7 @@ class ShoesTest < MiniTest::Unit::TestCase
   def test_range_rand
     assert_respond_to (1..5), :rand
   end
+  def test_shape
+    skip "Need to learn how to test shape"
+  end
 end
